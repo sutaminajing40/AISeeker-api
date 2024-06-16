bd09d02ac5dc9b3bd9312444be2fba9bb8835f75
Hiroto Iguchi <sutaminajing40@gmail.com>
2024-06-16 22:32:51 +0900

    メインUIを変更

--- packages/frontend/src/app/components/elements/input/Input.tsx
+++ packages/frontend/src/app/components/elements/input/Input.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Input.css'
+import Icon from '@mdi/react'
+import { mdiArrowRightBoldBox } from '@mdi/js'
 
 export function Input() {
   return (
-    <input
-      className="input"
-      type="text"
-      placeholder="知りたい質問を書いてください(⌘ + Enterで送信)"
-    />
+    <div className="input-container">
+      <input
+        className="input"
+        type="text"
+        placeholder="知りたい質問を書いてください(⌘ + Enterで送信)"
+      />
+      <Icon path={mdiArrowRightBoldBox} size={1} className="input-icon" />
+    </div>
   )
 }
