64db26150d7bacdead6d1c84e0bfe4f85a9949c2
Hiroto Iguchi <sutaminajing40@gmail.com>
2024-06-24 03:26:47 +0900

    サイドバーを途中まで作成

--- packages/frontend/src/features/home/sidebar/Sidebar.tsx
+++ packages/frontend/src/features/home/sidebar/Sidebar.tsx
@@ -1,5 +1,21 @@
+'use client'
+
 import './Sidebar.css'
+import Image from 'next/image'
+
+import { Button } from '@/app/components/elements/button/Button'
 
 export function Sidebar() {
-  return <aside className="sidebar">Sidebar</aside>
+  return (
+    <aside className="sidebar">
+      <header className="sidebar-header">
+        <Image src="/images/logo.png" alt="ロゴ" width={271} height={83} />
+        <Button />
+      </header>
+      <main className="sidebar-content"></main>
+      <footer className="sidebar-footer">
+        <div className="sidebar-footer-button"></div>
+      </footer>
+    </aside>
+  )
 }
